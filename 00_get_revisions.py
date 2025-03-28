7c7d486093d1fbb6541902e6ea52c39e3618a944
Neha Kennard <neha.kennard@gmail.com>
2025-03-28 10:42:53 -0400

    cleanup

--- 00_get_revisions.py
+++ 00_get_revisions.py
@@ -1,3 +1,7 @@
+"""Use OpenReview API to get initial and final PDFs for ICLR submissions.
+"""
+
+
 import argparse
 import collections
 import json
@@ -57,6 +61,16 @@ INVITATIONS = {
     for year in range(2018, 2025)
 }
 
+def is_review(note, conference):
+    if conference == scc_lib.Conference.iclr_2023:
+        return "Official_Review" in note.invitation
+    elif conference == scc_lib.Conference.iclr_2022:
+        return 'main_review' in note.content
+    elif conference == scc_lib.Conference.iclr_2024:
+        assert False
+    else:
+        return 'review' in note.content
+
 # == Other helpers ===========================================================
 
 Review = collections.namedtuple("Review",
@@ -69,6 +83,11 @@ class ForumStatus(object):
     NO_PDF = "no_pdf"
     NO_REVISION = "no_revision"
 
+def first_not_none(l):
+    for x in l:
+        if x is not None:
+            return x
+    assert False
 
 # ============================================================================
 
@@ -99,16 +118,26 @@ def get_last_valid_reference(references):
     return None, None
 
 
-def get_review_sentences_and_rating(note):
-    if 'review' in note.content:
-        review_text = note.content['review']
-        rating = note.content['rating']
-    elif 'main_review' in note.content:
+def get_review_sentences_and_rating(note, conference):
+    """Get raw review text. Review text field differs between years.
+    """
+    if conference == scc_lib.Conference.iclr_2023:
+        review_text = "\n".join(
+            [note.content[key]
+                for key in [
+                    "strengths_and_weaknesses"
+                    # TODO: find the other fields
+                ]]
+        rating = note.content['recommendation']
+    elif conference == scc_lib.Conference.iclr_2022:
         review_text = note.content['main_review']
         rating = note.content['recommendation']
+    elif conference == scc_lib.Conference.iclr_2024:
+        assert False
     else:
-        review_text = note.content['strength_and_weaknesses']
-        rating = note.content['recommendation']
+        review_text = note.content['review']
+        rating = note.content['rating']
+
     return [
         sent.text
         for sent in scc_lib.SENTENCIZE_PIPELINE(review_text).sentences
@@ -119,7 +148,8 @@ def write_metadata(forum_dir, forum, conference, initial_id, final_id,
                    decision, review_notes):
     reviews = []
     for review_note in review_notes:
-        review_sentences, rating = get_review_sentences_and_rating(review_note)
+        review_sentences, rating = get_review_sentences_and_rating(review_note,
+        conference)
         reviews.append(
             Review(review_note.id, review_sentences, rating,
                    export_signature(review_note),
@@ -141,53 +171,58 @@ def write_metadata(forum_dir, forum, conference, initial_id, final_id,
                 indent=2))
 
 
-def retrieve_forum(forum, conference, output_dir):
+def process_forum(forum, conference, output_dir):
+
+    # === Get all notes, reviews, decisions, etc ==============================
+
+    forum_notes = GUEST_CLIENT.get_all_notes(forum=forum.id)
 
     # Retrieve all reviews from the forum
-    review_notes = []
-    for note in GUEST_CLIENT.get_all_notes(forum=forum.id):
-        #print(note)
-        #print()
-        #print()
-        if (note.replyto == note.forum and
-            ("main_review" in note.content
-            or "review" in note.content
-            or "Official_Review" in note.invitation)):
-            # ICLR 2018 - 2021 have review as a field
-            # ICLR 2022 has main_review as a field
-            # Others have separate fields
-            review_notes.append(note)
-
+    review_notes = [
+        note for note in forum_notes if is_review(note, conference)
+        ]
+        # The conditions that make a note a review differ from year to year.
+        
     # Retrieve decision
-    decision = 'none'
-    for note in GUEST_CLIENT.get_all_notes(forum=forum.id):
-        if 'decision' in note.content.keys():
-            decision = note.content['decision']
-            break
+    decisions = first_not_none([
+        note.content.get('decision', None)
+        for note in forum_notes
+    ])
 
     # e.g. If the paper was withdrawn
     if not review_notes:
         return ForumStatus.NO_REVIEWS, decision
 
-    # Earliest creation time out of all the reviews. Changes made before this
-    # cannot have been influenced by reviewers.
-    first_review_time = min(rev.tcdate for rev in review_notes)
+    # === Get `initial' and `final' pdfs ======================================
 
-    # Retrieve all revisions of the manuscript
+    # Retrieve all revisions of the manuscript in order of submission
     references = sorted(GUEST_CLIENT.get_all_references(referent=forum.id,
                                                         original=True),
                         key=lambda x: x.tcdate)
 
-    # The 'final' version is the latest version associated with a valid PDF
+    # Valid references are those associated with a valid PDF.
+
+    # --- final submission ----------------------------------------------------
+    # The latest valid revision
+
+    # The 'final' version is the latest valid version
     final_reference, final_binary = get_last_valid_reference(references)
 
-    # The 'initial' version is the last version associated with a valid PDF
-    # that was created before the first review
+    # --- initial submission --------------------------------------------------
+    # The latest valid pre-review revision
+
+    # Creation time of first review:
+    # Changes made before this time cannot have been influenced by reviewers.
+    first_review_time = min(rev.tcdate for rev in review_notes)
+
     references_before_review = [
         r for r in references if r.tcdate <= first_review_time
     ]
+    # The initial revision is the latest valid revision of the list above
     initial_reference, initial_binary = get_last_valid_reference(
         references_before_review)
+
+    # === Finalize ============================================================
 
     # Proceed only for forums with valid and distinct initial and final
     # versions
@@ -216,22 +251,35 @@ def main():
 
     args = parser.parse_args()
 
+    # A directory will be made for each paper submission under the output directory.
     os.makedirs(args.output_dir, exist_ok=True)
+
     statuses = []
+    
+    # Gets top level notes for each `forum' (each paper submission is assigned
+    # a forum)
     forum_notes = GUEST_CLIENT.get_all_notes(
         invitation=INVITATIONS[args.conference])
-    print(INVITATIONS[args.conference])
-    print(len(forum_notes))
+
+    # Hack for --debug    
     success_count = 0
+
     for forum in tqdm.tqdm(forum_notes):
-        status, decision = retrieve_forum(forum, args.conference,
+        # Process a forum. As a side effect, write pdfs to directory.
+        status, decision = process_forum(forum, args.conference,
                                           args.output_dir)
         statuses.append((forum.id, status, decision))
+    
+        # === --debug stuff ===
         if status == ForumStatus.COMPLETE:
             success_count += 1
         if args.debug and success_count == 10 or len(statuses) > 100:
             break
+        # === end --debug stuff ===
 
+
+    # Write all statuses to file.
+    # TODO: determine if useful to do while processing rather than at the end.
     with open(f'{args.status_file_prefix}{args.conference}.tsv', 'w') as f:
         f.write('#Conference\tForum\tStatus\tDecision\n')
         for forum, status, decision in statuses:
