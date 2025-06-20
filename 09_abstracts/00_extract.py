97a5fdf86b3f1eebca498ab0b83738f2406b97ab
Neha Kennard <nnayak@umass.edu>
2025-06-20 18:38:26 +0000

    complete with error reporting

--- 09_abstracts/00_extract.py
+++ 09_abstracts/00_extract.py
@@ -1,8 +1,7 @@
 import argparse
 import collections
 import glob
-import json
-import os
+#import json
 import re
 import tqdm
 
@@ -10,95 +9,109 @@ import new_scc_lib
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("-d", "--data_dir", default="", type=str, help="")
-parser.add_argument("-c",
-                    "--conference",
-                    type=str,
-                    choices=new_scc_lib.Conference.ALL,
-                    help="conference_year, e.g. iclr_2022",
-                    required=True)
 
-
-VERSION_INFO_FIELDS = "title abstract intro sec2".split()
-
+VERSION_INFO_FIELDS = "title abstract intro debug_next_sec".split()
 VersionInfo = collections.namedtuple("VersionInfo", VERSION_INFO_FIELDS)
 
-def parse_initial(initial_text, forum_id):
-    title = initial_text.split("\n")[0]
-    if 'Anonymous' in title:
-        r = re.search("Anonymous", title).span()[0]
-        title = title[:r]
-    initial_text = initial_text.replace("\n", "").strip()
-    if not initial_text:
-        print(f'{forum_id}\tEmpty file')
-        return None
-    try:
-        pre_abs, post_abs = initial_text.split("ABSTRACT", 1)
-    except ValueError:
-        try:
-            pre_abs, post_abs = initial_text.split("Abstract", 1)
-        except ValueError:
-            print(f'{forum_id}\tMaybe no abstract')
-            return None
-    try:
-        maybe_abs, post_int = post_abs.split("1 INTRODUCTION", 1)
-    except ValueError:
-        try:
-            maybe_abs, post_int = post_abs.split("1 Introduction", 1)
-        except ValueError:
-            print(f'{forum_id}\tMaybe no introduction')
-            return None
-    r = re.search("2\s[A-Z]+", post_int).span()[0]
-    maybe_int = post_int[:r]
-    return VersionInfo(title, maybe_abs, maybe_int, post_int[r:r+100])
+PAPER_INFO_FIELDS = "conference forum_id initial_info final_info".split()
+PaperInfo = collections.namedtuple("PaperInfo", PAPER_INFO_FIELDS)
+
+ParseError = collections.namedtuple("ParseError",
+                                    "hint forum_id version".split())
 
 
 def subtitle_splitter(subtitle_strings, text):
-    for subtitle in substitle_strings:
+    """Split text into before and after a subheading."""
+    for subtitle in subtitle_strings:
         if subtitle in text:
             try:
-               pre, post = text.split(subtitle, 1)
-               return pre, post
+                pre, post = text.split(subtitle, 1)
+                return pre.strip(), post.strip()
             except ValueError:
-                print(f"Cannot find {subtitle}")
                 return None
     return None
 
 
-def parse_final(final_text, forum_id):
-    final_text = final_text.strip()
-    title = final_text.split("\n")[0]
-    if 'Anonymous' in title:
-        r = re.search("Anonymous", title).span()[0]
-        title = title[:r]
+def parse_extracted_iclr_text(filename, metadata):
+
+    with open(filename, 'r') as f:
+        text = f.read().strip()
+
+    if not text:
+        return ParseError("Empty file", *metadata)
+
+    # Text usually starts with the title in all caps.
+    # Either the title is on its own line or the authors (either named or
+    # anonymous) are on the same line after the title.
+    maybe_title = text.split("\n")[0]
+    if 'Anonymous' in maybe_title:  # Split before 'Anonymous'
+        r = re.search("Anonymous", maybe_title).span()[0]
+        title = maybe_title[:r]
     else:
-        maybe_name_start = re.search("[A-Z][a-z]", title)
-        if maybe_name_start is not None:
-            title = title[:maybe_name_start.span()[0]]
-    print(title)
-    final_text = final_text.replace("\n", "").strip()
+        maybe_name_start = re.search("[A-Z][a-z]", maybe_title)
+        if maybe_name_start is not None:  # A name-looking substring occurs
+            title = maybe_title[:maybe_name_start.span()[0]]
+        else:
+            title = maybe_title  # Title occurs on its own line?
 
-    pre_abs, post_abs = subtitle_splitter(["Abstract", "ABSTRACT"], final_text)
-    abstract, post_int = subtitle_splitter(["1 Introduction", "1 INTRODUCTION"],
-    final_text)
+    # Most papers have either 'Abstract' or 'ABSTRACT' pretty reliably
+    try:
+        pre_abs, post_abs = subtitle_splitter(["Abstract", "ABSTRACT"], text)
+    except TypeError:  # Because it returned None
+        # Sometimes this fails but it's rare
+        return ParseError("Finding Abstract heading", *metadata)
+
+    # Try to find the first section which should be Introduction
+    try:
+        abstract, post_int = subtitle_splitter(
+            ["1 Introduction", "1 INTRODUCTION"], post_abs)
+    except TypeError:  # Because it returned None
+        # Sometimes there is text before the first section starts, or the first
+        # section is called something besides introduction, like "Motivation"
+        return ParseError("Finding Introduction heading", *metadata)
+
+    # Find the start of the second section in order to delimit the
+    # introduction.
     next_sec_offset = re.search("2\s[A-Z]+", post_int).span()[0]
-    introduction = post_int[:r]
-    return VersionInfo(title, maybe_abs, maybe_int, post_int[r:r+100])
+    introduction = post_int[:next_sec_offset]
+    next_sec = post_int[next_sec_offset:next_sec_offset + 50]
 
+    return VersionInfo(title, abstract, introduction, next_sec)
 
 
 def main():
     args = parser.parse_args()
     papers = []
-    for initial_filename in tqdm.tqdm(
-            list(glob.glob(
-                f"{args.data_dir}/{args.conference}/*/initial.txt"))[:10]):
-        forum_id = initial_filename.split("/")[-2]
-        with open(initial_filename, 'r') as f:
-            initial_info = parse_initial(f.read(), forum_id)
-        final_filename = initial_filename.replace("initial", "final")
-        with open(final_filename, 'r') as f:
-            final_info = parse_final(f.read(), forum_id)
-            print("=" * 80)
+    errors = []
+    error_count = 0
+    for conference in new_scc_lib.Conference.ALL:
+        for initial_filename in tqdm.tqdm(
+                list(
+                    glob.glob(f"{args.data_dir}/{conference}/*/initial.txt"))):
+            forum_id = initial_filename.split("/")[-2]
+            initial_info = parse_extracted_iclr_text(
+                initial_filename, (forum_id, new_scc_lib.INITIAL))
+            final_info = parse_extracted_iclr_text(
+                initial_filename.replace('initial', 'final'),
+                (forum_id, new_scc_lib.FINAL))
+
+            if isinstance(initial_info, VersionInfo) and isinstance(
+                    final_info, VersionInfo):
+                papers.append(
+                    PaperInfo(conference, forum_id, initial_info._asdict(),
+                              final_info._asdict()))
+            else:
+                for info in [initial_info, final_info]:
+                    if isinstance(info, ParseError):
+                        errors.append(info)
+                error_count += 1
+
+    print(
+        f'Num errors: {error_count} ({len(errors)}); Parsed papers: {len(papers)}'
+    )
+    new_scc_lib.namedtuples_to_jsonl(papers, 'abstract_intro_versions.jsonl')
+    new_scc_lib.namedtuples_to_jsonl(errors, 'errors.jsonl')
+
 
 if __name__ == "__main__":
     main()
